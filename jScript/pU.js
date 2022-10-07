0e07bf2a6c6c0d5bb48f152d79a2d61bef7874b7
eldictador007 <chantilli@protonmail.ch>
2022-10-07 19:35:58 -0300

    first commit

---
+++ jScript/pU.js
@@ -0,0 +1,12 @@
+
+
+// arma2Pc 0.1 (2022.10.07)
+
+function ClientId (name,age,data) {
+    this.name=name
+    this.age=age
+    this=data=data
+}
+const client1=new ClientId(" ",0,"X")
+const client2=new ClientId("  ",9)
+const client3=new ClientId(" ",1)
